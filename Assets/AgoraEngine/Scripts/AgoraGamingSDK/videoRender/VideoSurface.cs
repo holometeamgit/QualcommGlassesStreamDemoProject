1eabc96940d4f0be72a6dfa5808ef56031062415
Zuheb <zuheb.javed@holome.co.uk>
2023-02-27 17:47:25 +0000

    -Added splash screens and app logo -Updated unity version to stop glitches -Updated Spaces SDK

--- Assets/AgoraEngine/Scripts/AgoraGamingSDK/videoRender/VideoSurface.cs
+++ Assets/AgoraEngine/Scripts/AgoraGamingSDK/videoRender/VideoSurface.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fe2794bbc47c5e67edd81fdbdf98f847bc53cd8ee6ab9b1a767085d8c0c8a99
-size 15459
+oid sha256:3293f2c14d8ea0510e67f5377e1586b92ca96bcfdbc1b635530872b9289a11f5
+size 15465
